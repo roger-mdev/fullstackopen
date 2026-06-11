c547075d506d092811ea5757b3d32b7b861008b6
roger-mdev <rogermatthewgoetsch@gmail.com>
2026-06-11 19:49:44 +0200

    "completed Phonebook app"

--- part2/The-Phonebook/src/App.jsx
+++ part2/The-Phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import personServices from './services/persons'
 import Persons from './components/Persons'
 import Phonebook from './components/Phonebook'
 import PersonForm from './components/PersonForm'
+import Status from './components/Status'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -10,6 +11,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setSearch] = useState('')
+  const [status, setStatus] = useState({type: "success" , message: "nothing to report"})
 
   useEffect(() => {
     personServices.getAll()
@@ -19,6 +21,7 @@ const App = () => {
       })
   } ,[])
 
+  const setDefaultStatus = () => setStatus({type: 'success', message: 'nothing to report'})
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -42,9 +45,13 @@ const App = () => {
       number: newNumber
     }
 
-    if (persons.some(person => person.name === newName) 
-      && persons.some(person => person.number === newNumber)) {
-        alert(`${newName} already has ${newNumber} in the phonebook`)
+    if (persons.some(person => person.number === newNumber)) {
+        setStatus({
+          type: "failure",
+          message: 
+          `${persons.find(person => person.number === newNumber).name} already has ${newNumber} in the phonebook`
+        })
+        setTimeout(setDefaultStatus, 5000)
   } else if (persons.some(person => person.name === newName) 
       && persons.some(person => person.number !== newNumber)){
         console.log(persons.find(person => person.name === newName).id)
@@ -59,21 +66,37 @@ const App = () => {
               setPersons(persons.map(person => person.id === response.id 
                 ? response : person
               ))
+              setStatus({
+                type: "success",
+                message: `added ${newNumber} to ${newName}`})
+              setTimeout(setDefaultStatus, 3000)
               setNewName('')
               setNewNumber('')
           })
             .catch(error => {
               console.log(error)
+              setPersons(persons.filter(person => person.name !== newName))
+              setStatus({
+              type: "failure",
+              message: `Information of ${newName} 
+                has already been removed from the server`})
+              setTimeout(setDefaultStatus, 3000)
             })
         } else {
-          alert(`${newName}'s number remains unchanged`)
+          setStatus({
+            type: "failure",
+            message: `${newName}'s number remains unchanged`})
+          setTimeout(setDefaultStatus, 3000)
         }
   } else {
       personServices
         .create(newPerson)
         .then(response => {
           console.log(response)
-
+          setStatus({
+            type: "success",
+            message: `added ${newName}`})
+          setTimeout(setDefaultStatus, 3000)
           setPersons(persons.concat(response))
           setNewName('')
           setNewNumber('')
@@ -87,16 +110,32 @@ const App = () => {
       .then(response => {
         console.log(response)
         setPersons(persons.filter(person => person.id !== response.id))
+        setStatus({
+        type: "success",
+        message: `Information of ${persons.find(person => person.id === id).name} 
+          has been removed from the server`})
+        setTimeout(setDefaultStatus, 3000)
       })
-      .catch(error => console.log(error))
+      .catch(error => {
+        setPersons(persons.filter(person => person.id !== response.id))
+        setStatus({
+        type: "failure",
+        message: `Information of ${persons.find(person => person.id === id).name} 
+          has already been removed from the server`})
+        setTimeout(setDefaultStatus, 3000)
+      })
     } else {
-      alert(`Removal of ${persons.find(person => person.id === id).name} canceled`)
+      setStatus({
+        type: "failure",
+        message: `Removal of ${persons.find(person => person.id === id).name} canceled`})
+      setTimeout(setDefaultStatus, 3000)
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Status type={status.type} message={status.message}/>
       <Phonebook search={newSearch} change={handleSearchChange} />
       <h2>add a new Person</h2>
       <PersonForm name={newName} number={newNumber} 
