c0cde1d1cb3ad00a49190d3271e6a93bd2a4f20f
roger-mdev <rogermatthegoetsch@gmail.com>
2026-08-07 10:36:35 +0200

    added models dir

---
+++ part3/phonebookBackend/models/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log(`connecting to ${url}`)
+mongoose.connect(url, {family: 4})
+  .then(result => {
+    console.log(`connected to mongodb`)
+  })
+  .catch(error => {
+    console.log(`error connecting to mongodb ${error.message}`)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString()
+    delete returnObject._id
+    delete returnObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
