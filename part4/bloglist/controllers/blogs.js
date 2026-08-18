716ae1e6ee063daec7fe9b03d812b99a6065112a
roger-mdev <rogermatthegoetsch@gmail.com>
2026-08-18 11:00:12 +0200

    part 4.2 files added

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,20 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+blogsRouter.get('/', (req, res) => {
+  Blog.find({}).then(blogs => {
+    res.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (req, res) => {
+  const blog = new Blog(req.body)
+  logger.info(req.body)
+
+  blog.save().then((result) => {
+    res.status(201).json(result)
+  })
+})
+
+module.exports = blogsRouter
