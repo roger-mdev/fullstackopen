42807c8253a2eead01d6fbbd12b0ea1f0cb9b760
roger-mdev <rogermatthegoetsch@gmail.com>
2026-08-25 12:43:30 +0200

    completed 4a

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -18,7 +18,55 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(reducer)
 }
 
+const mostBlogs = (blogs) => {
+  const authors = []
+  blogs.forEach(blog => {
+    let index = authors.findIndex(author => author.author === blog.author)
+    if (-1 === index) {
+      authors.push({
+        author: blog.author,
+        blogs: 1
+      })
+    } else {
+      authors[index].blogs += 1
+    }
+  })
+  
+  const reducer = (mostIndex, curr, index) => {
+    return authors[mostIndex].blogs >= curr.blogs
+      ? mostIndex
+      : index
+  }
+  
+  return authors[authors.reduce(reducer, 0)]
+}
+
+const authorMostLIkes = (blogs) => {
+  const authors = []
+    blogs.forEach(blog => {
+    let index = authors.findIndex(author => author.author === blog.author)
+    if (-1 === index) {
+      authors.push({
+        author: blog.author,
+        likes: blog.likes
+      })
+    } else {
+      authors[index].likes += blog.likes
+    }
+  })
+
+  const reducer = (mostIndex, curr, index) => {
+    return authors[mostIndex].likes >= curr.likes
+      ? mostIndex
+      : index
+  }
+
+  return authors[authors.reduce(reducer, 0)]
+}
+
 module.exports = {
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  authorMostLIkes,
 }
